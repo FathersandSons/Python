13fbacd702b5998e0e572a6330566a7e8aa3a9b5
FathersandSons <112703407+FathersandSons@users.noreply.github.com>
2022-11-14 05:56:19 -0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+z = str(3) #will be "3" or '3' - "" and '' are the same
+t = float(8) #will be 8.0
+
+print(type(5))
+print(type(y))
+print(type(z))
+print(type(t))
+
+myVariable = "Sally"
+print(myVariable)
