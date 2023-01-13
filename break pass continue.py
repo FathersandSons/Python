0790a8383abfe7e03bbfc585d5e11a5baa75ebef
FathersandSons <112703407+FathersandSons@users.noreply.github.com>
2023-01-13 03:33:15 -0800

    Add files via upload

---
+++ break pass continue.py
@@ -0,0 +1,5 @@
+for sayi in range(1,10,1):
+    if sayi==5:
+        pass ##eger break pass ve continue'nin nasil calistigini gormek istiyoesan pass yerine onlari yaz.
+    print("sayi, {}".format(sayi))
+print("dongu bitmistir")
