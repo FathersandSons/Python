e5acd868ba0596147223313b34898891e5a29741
FathersandSons <112703407+FathersandSons@users.noreply.github.com>
2023-01-16 08:46:43 -0800

    Add files via upload

---
+++ daire alan format.py
@@ -0,0 +1,11 @@
+r=float(input("lutfen yaricap girin...\t"))
+pi=3.14
+alan=(pi*r**2)
+kureninHacmi=(4/3*pi*r**3)
+cevre=(2*pi*r)
+kureninAlani=(4*pi*r**2)
+print("""yaricapi {} olan dairenin cevresi:{}
+dairenin alani:{}
+kurenin alani:{}
+kurenin hacmi:{}
+""".format(r,cevre,alan,kureninAlani,kureninHacmi))
