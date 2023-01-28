4c31bcb367ae188f57050e7822d4788393b2f40a
FathersandSons <112703407+FathersandSons@users.noreply.github.com>
2023-01-28 02:34:05 -0800

    Add files via upload

---
+++ endswith fonksiyonu.py
@@ -0,0 +1,6 @@
+##endswith boyle mi bitiyor? True False
+web=input("lutfen internet sitenizi giriniz...\t")
+if web.endswith("www.") or web.endswith("http//www."):
+    print("tesekkurler")
+else:
+    print("hatali giris")
